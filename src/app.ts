e2fffadba504ae9fce119bc81f859dd5d0623815
KEVISH23 <kevishthakkar001@gmail.com>
2024-05-27 18:46:18 +0530

    link for api docs in app.ts

--- src/app.ts
+++ src/app.ts
@@ -14,3 +14,5 @@ server.setConfig(async ()=>{
     await dbConnect()
 })
 server.build().listen(config.get('port'),()=>console.log('port connection established'))
+
+// link for api documentation:- https://documenter.getpostman.com/view/34315314/2sA3Qs9rW2
